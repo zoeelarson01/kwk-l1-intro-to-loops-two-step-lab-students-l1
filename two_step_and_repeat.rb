17e6c056712875b4e27da70f1a96a36fb35e0b26
zoeelarson01 <zoeelarson@gmail.com>
2018-06-05 21:01:11 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,37 +1,56 @@
-def first_steps
-  loop do
-    puts "Right foot back"
-    sleep (0.5)
-    puts "Left foot back"
-    sleep (0.5)
-    puts "Right foot back"
-    sleep (0.5)
-    puts "Stop"
-    sleep (1)
-  end
-end
+# def first_steps
+#   loop do
+#     puts "Right foot back"
+#     sleep (0.5)
+#     puts "Left foot back"
+#     sleep (0.5)
+#     puts "Right foot back"
+#     sleep (0.5)
+#     puts "Stop"
+#     sleep (1)
+#   end
+# end
 
-def a_few_more_steps
-  loop do
-    puts "Right foot back"
-    sleep (0.5)
-    puts "Left foot back"
-    sleep (0.5)
-    puts "Right foot back"
-    sleep (0.5)
-    puts "Stop"
-    sleep (1)
-    puts "Right foot steps right and back"
-    sleep (0.5)
-    puts "Left foot crosses over right"
-    sleep (0.5)
-    puts "Right foot steps right"
-    sleep (0.5)
-    puts "Turn"
-    sleep (1)
-  end
-end
+# def a_few_more_steps
+#   loop do
+#     puts "Right foot back"
+#     sleep (0.5)
+#     puts "Left foot back"
+#     sleep (0.5)
+#     puts "Right foot back"
+#     sleep (0.5)
+#     puts "Stop"
+#     sleep (1)
+#     puts "Right foot steps right and back"
+#     sleep (0.5)
+#     puts "Left foot crosses over right"
+#     sleep (0.5)
+#     puts "Right foot steps right"
+#     sleep (0.5)
+#     puts "Turn"
+#     sleep (1)
+#   end
+# end
 
+# def how_many_steps?
+#   steps = 0
+#   loop do
+#     steps += 1
+#     puts steps
+#     if steps % 2 == 0 
+#       puts "Left"
+#     else
+#       puts "Right"
+#     end
+#     sleep (0.5)
+#   end
+# end
+  
+
+#   # Write a loop that fulfills all criteria from Part 3 of the README.md
+# end
+
+# def break_dance
 def how_many_steps?
   steps = 0
   loop do
@@ -43,14 +62,10 @@ def how_many_steps?
       puts "Right"
     end
     sleep (0.5)
+    if steps = 6, break
   end
 end
-  
 
-#   # Write a loop that fulfills all criteria from Part 3 of the README.md
-# end
-
-# def break_dance
 #   # Write a solution that uses the same code as how_many_steps?, but breaks the
 #   # loop if steps is equal to 6
 # end
