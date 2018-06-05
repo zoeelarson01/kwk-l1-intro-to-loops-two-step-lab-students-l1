229ac8f6c986c55b8d577eeed6b95c4bafd14519
zoeelarson01 <zoeelarson@gmail.com>
2018-06-05 20:59:44 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,36 +1,36 @@
-# def first_steps
-#   loop do
-#     puts "Right foot back"
-#     sleep (0.5)
-#     puts "Left foot back"
-#     sleep (0.5)
-#     puts "Right foot back"
-#     sleep (0.5)
-#     puts "Stop"
-#     sleep (1)
-#   end
-# end
+def first_steps
+  loop do
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Left foot back"
+    sleep (0.5)
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Stop"
+    sleep (1)
+  end
+end
 
-# def a_few_more_steps
-#   loop do
-#     puts "Right foot back"
-#     sleep (0.5)
-#     puts "Left foot back"
-#     sleep (0.5)
-#     puts "Right foot back"
-#     sleep (0.5)
-#     puts "Stop"
-#     sleep (1)
-#     puts "Right foot steps right and back"
-#     sleep (0.5)
-#     puts "Left foot crosses over right"
-#     sleep (0.5)
-#     puts "Right foot steps right"
-#     sleep (0.5)
-#     puts "Turn"
-#     sleep (1)
-#   end
-# end
+def a_few_more_steps
+  loop do
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Left foot back"
+    sleep (0.5)
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Stop"
+    sleep (1)
+    puts "Right foot steps right and back"
+    sleep (0.5)
+    puts "Left foot crosses over right"
+    sleep (0.5)
+    puts "Right foot steps right"
+    sleep (0.5)
+    puts "Turn"
+    sleep (1)
+  end
+end
 
 def how_many_steps?
   steps = 0
@@ -42,11 +42,10 @@ def how_many_steps?
     else
       puts "Right"
     end
-    break
+    sleep (0.5)
   end
 end
   
-how_many_steps?
 
 #   # Write a loop that fulfills all criteria from Part 3 of the README.md
 # end
